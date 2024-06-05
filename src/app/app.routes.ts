f640dc9c360583ba91c36ea193ffc7c80ec5b3eb
Luk <Lukasz.jablonski1991@gmail.com>
2024-06-05 18:16:24 +0200

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ReaderComponent } from './reader/reader.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ReaderComponent,
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent),
+    title: 'Admin',
+  },
+];
