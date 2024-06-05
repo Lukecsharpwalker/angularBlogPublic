299374a280294320bfa8bcf09fc3efd48e06be59
Luk <Lukasz.jablonski1991@gmail.com>
2024-06-05 15:27:08 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyAV55u4H2C0i8n0DUl5Ym_cJYjOl0NwQck",
+    authDomain: "angularblog-267d7.firebaseapp.com",
+    projectId: "angularblog-267d7",
+    storageBucket: "angularblog-267d7.appspot.com",
+    messagingSenderId: "247576047093",
+    appId: "1:247576047093:web:e9d17af5c6472c101684bc",
+    measurementId: "G-QEE6VH1NP0"
+  }
+};
