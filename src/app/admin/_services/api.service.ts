ab739b9bda68192c26d5270961ef91a7e6eebc98
Luk <Lukasz.jablonski1991@gmail.com>
2024-06-06 16:02:11 +0200

    nothing special

--- src/app/admin/_services/api.service.ts
+++ src/app/admin/_services/api.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, doc, setDoc } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, doc, setDoc } from '@angular/fire/firestore';
 import { Blog } from '../../shared/_models/blog.interface';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ApiService {
@@ -10,8 +11,8 @@ export class ApiService {
 
   addBlog(blog: Blog) {
     const newTaskRef = doc(collection(this.firestore, 'blog'));
-    setDoc(newTaskRef, blog);
+     setDoc(newTaskRef, blog).finally(() => {
+      console.log('Document written with ID: ', newTaskRef.id);
+    });
   }
-
-
 }
