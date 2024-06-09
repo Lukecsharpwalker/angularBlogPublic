bd74a44886ceac4613b678d042a5fd4d2ab19b83
Luk <Lukasz.jablonski1991@gmail.com>
2024-06-09 16:29:54 +0200

    hydration fails

--- src/app/reader/_components/main-page/main-page.component.ts
+++ src/app/reader/_components/main-page/main-page.component.ts
@@ -1,22 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ReaderApiService } from '../../_services/reader-api.service';
-import { FirestoreModule } from '@angular/fire/firestore';
-import { AsyncPipe } from '@angular/common';
-import { RouterLink, RouterModule } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Post } from '../../../shared/_models/post.interface';
-import { PostComponent } from './post/post.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [FirestoreModule, AsyncPipe, RouterLink, PostComponent, RouterModule],
-  providers: [ReaderApiService],
+  imports: [RouterModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainPageComponent {
-  apiService = inject(ReaderApiService);
-  posts$: Observable<Post[]> = this.apiService.posts$;
-}
+export class MainPageComponent {}
