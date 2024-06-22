5d05ed68da285f18c19da74326d2985ccb25e250
Luk <Lukasz.jablonski1991@gmail.com>
2024-06-22 21:03:49 +0200

    auth guard fixed

--- src/app/shared/_guards/auth.guard.ts
+++ src/app/shared/_guards/auth.guard.ts
@@ -1,14 +1,20 @@
 import { inject } from '@angular/core';
 import { CanMatchFn, Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
-export const authGuard: CanMatchFn = () => {
+export const authGuard: CanMatchFn = (): Observable<boolean> => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  if (authService.isAdmin$()) {
-    return true;
-  } else {
-    router.navigate(['/posts']);
-    return false;
-  }
+
+  return authService.getAdminStatus().pipe(
+    map(isAdmin => {
+      if (!isAdmin) {
+        router.navigate(['/posts']);
+      }
+      return isAdmin;
+    }),
+    take(1)
+  );
 };
