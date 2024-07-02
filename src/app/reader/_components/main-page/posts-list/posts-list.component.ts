e067141ce4abbbc68a8e415d07545561f98c8dae
Luk <Lukasz.jablonski1991@gmail.com>
2024-07-02 20:23:37 +0200

    pure presentional card

--- src/app/reader/_components/main-page/posts-list/posts-list.component.ts
+++ src/app/reader/_components/main-page/posts-list/posts-list.component.ts
@@ -1,21 +1,22 @@
 import { AsyncPipe, NgClass } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FirestoreModule } from '@angular/fire/firestore';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Post } from '../../../../shared/_models/post.interface';
 import { ReaderApiService } from '../../../_services/reader-api.service';
 import { PostComponent } from '../post/post.component';
-import { PostCardComponent } from '../../presentational-components/post-card/post-card.component';
+import { CardComponent } from '../../presentational-components/post-card/card.component';
 
 @Component({
   selector: 'app-posts-list',
   standalone: true,
-  imports: [FirestoreModule, AsyncPipe, RouterLink, PostComponent, PostCardComponent, NgClass],
+  imports: [FirestoreModule, AsyncPipe, RouterLink, PostComponent, CardComponent, NgClass, RouterModule],
   providers: [ReaderApiService],
   templateUrl: './posts-list.component.html',
   styleUrl: './posts-list.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PostsListComponent {
   apiService = inject(ReaderApiService);
