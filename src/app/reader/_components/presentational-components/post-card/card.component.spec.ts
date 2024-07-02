f21a271554618f5ef92733b2d2c9caa31854c334
Luk <Lukasz.jablonski1991@gmail.com>
2024-07-02 20:23:37 +0200

    pure presentional card

--- src/app/reader/_components/presentational-components/post-card/card.component.spec.ts
+++ src/app/reader/_components/presentational-components/post-card/card.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PostCardComponent } from './post-card.component';
+import { CardComponent } from './card.component';
 
-describe('PostCardComponent', () => {
-  let component: PostCardComponent;
-  let fixture: ComponentFixture<PostCardComponent>;
+describe('CardComponent', () => {
+  let component: CardComponent;
+  let fixture: ComponentFixture<CardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PostCardComponent]
+      imports: [CardComponent]
     })
     .compileComponents();
-    
-    fixture = TestBed.createComponent(PostCardComponent);
+
+    fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
