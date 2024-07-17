36e3d2be5772b3d763c262ed0d281b15c2734851
Lukecsharpwalker <34306549+Lukecsharpwalker@users.noreply.github.com>
2024-07-17 16:20:52 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -19,7 +19,7 @@ async function fetchPosts() {
     }
     const data: [{id:string}] = await response.json();
     const postIds = data.map(post => post.id);
-    const routes = ['/posts', '/posts/', ...postIds.map(id => `/posts/details${id}`)];
+    const routes = ['/','', ...postIds.map(id => `/posts/details${id}`)];
 
     fs.writeFileSync('routes.txt', routes.join('\n'), 'utf8');
 
